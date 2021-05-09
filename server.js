dec5cf75ee0e85bf326da4e0ae1b34d641c9e9b5
PaulB-H <p.bernardhall@gmail.com>
2021-05-08 23:43:03 -0400

    Add genTotp route & users array

--- server.js
+++ server.js
@@ -10,4 +10,17 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
+const users = [];
+
+app.post("/genTotp", (req, res) => {
+  const secret = speakeasy.generateSecret();
+
+  let key = users.length + 1;
+  users.push({ user: key, secret: secret.base32 });
+
+  QRCode.toDataURL(secret.otpauth_url, function (err, data_url) {
+    res.json({ qrcode_url: data_url, user: key });
+  });
+});
+
 app.listen(5000, () => console.log(`Server started on port ${5000}`));
