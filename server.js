3dafb8e8edab4406c0c48ee7fe2257cd9920bbe4
PaulB-H <p.bernardhall@gmail.com>
2021-05-08 23:38:02 -0400

    Add route for static index, add app.listen()

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const path = require("path");
 const app = express();
 const speakeasy = require("speakeasy");
 const QRCode = require("qrcode");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
+app.listen(5000, () => console.log(`Server started on port ${5000}`));
